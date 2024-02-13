8fb3197232715e5ccb916897c0cccb32019513d6
lana-z <lana@levelupeconomy.com>
2024-02-12 17:01:06 -0800

    project setup completed with django and tailwind

--- snacks/views.py
+++ snacks/views.py
@@ -3,5 +3,23 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["snacks"] = [
+            {
+                "name": "Carrots and hummus",
+                "description": "Vitamin A from beta-carotene",
+            }, {
+                "name": "Dark chocolate",
+                "description": "Iron and magnesium",
+            }, {
+                "name": "Almonds",
+                "description": "Vitamin E",
+            },
+        ]
+    
+        return context
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'compressor',
+
     'snacks',
 ]
 
@@ -125,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',) 
